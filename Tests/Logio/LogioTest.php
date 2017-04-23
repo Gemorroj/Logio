4fe9369019a1b9a24e6ca6d5b7044bcb456a40ee
Gemorroj <wapinet@gmail.com>
2017-04-23 13:19:10 +0300

    base structure

---
+++ Tests/Logio/LogioTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tests\Logio;
+
+class LogioTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSkip()
+    {
+        $this->markTestSkipped('Tests not implemented yet');
+    }
+}
---
+++ src/Logio/Logio.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Logio;
+
+class Logio
+{
+
+}
